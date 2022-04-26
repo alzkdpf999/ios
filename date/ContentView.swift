0fbc877c78de66762536eb8ccf05f95c459eca66
alzkdpf999 <alzkdpf999@naver.com>
2022-04-26 13:50:48 +0900

    Initial Commit

---
+++ date/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  date
+//
+//  Created by 정충효 on 2022/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ date/dateApp.swift
@@ -0,0 +1,17 @@
+//
+//  dateApp.swift
+//  date
+//
+//  Created by 정충효 on 2022/04/26.
+//
+
+import SwiftUI
+
+@main
+struct dateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
